fdb9102218b2dcef5e7089e18c66f0644d5a851b
wbail <guilhermedanbail@gmail.com>
2022-02-28 10:14:26 +0000

    feat: add rabbitmq

---
+++ Play.Common/Play.Common/Extensions/MassTransitRabbitMqExtension.cs
@@ -0,0 +1,32 @@
+﻿using MassTransit;
+using MassTransit.Definition;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Play.Common.Settings;
+using System.Reflection;
+
+namespace Play.Common.Extensions;
+
+public static class MassTransitRabbitMqExtension
+{
+    public static IServiceCollection AddMassTransitRabbitMqConfiguration(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddMassTransit(configure =>
+        {
+            configure.AddConsumers(Assembly.GetEntryAssembly());
+
+            configure.UsingRabbitMq((context, configurator) =>
+            {
+                var serviceSettings = configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
+
+                var rabbitMqSettings = configuration.GetSection(nameof(RabbitMqSettings)).Get<RabbitMqSettings>();
+                configurator.Host(rabbitMqSettings.Host);
+                configurator.ConfigureEndpoints(context, new KebabCaseEndpointNameFormatter(serviceSettings.ServiceName, false));
+            });
+        });
+
+        services.AddMassTransitHostedService();
+
+        return services;
+    }
+}
---
+++ Play.Common/Play.Common/Settings/RabbitMqSettings.cs
@@ -0,0 +1,6 @@
+﻿namespace Play.Common.Settings;
+
+public class RabbitMqSettings
+{
+    public string Host { get; init; }
+}
